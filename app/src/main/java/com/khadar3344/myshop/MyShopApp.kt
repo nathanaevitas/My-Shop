39f37352d91ad9ef5a5bdbbac007d52f5d89ec58
Nguyen Tran <nguyentlt.c3@gmail.com>
2024-02-20 19:34:18 +0700

    integrate appsflyer SDK

--- app/src/main/java/com/khadar3344/myshop/MyShopApp.kt
+++ app/src/main/java/com/khadar3344/myshop/MyShopApp.kt
@@ -1,7 +1,37 @@
 package com.khadar3344.myshop
 
 import android.app.Application
+import android.util.Log
+import com.appsflyer.AppsFlyerLib
+import com.appsflyer.attribution.AppsFlyerRequestListener
 import dagger.hilt.android.HiltAndroidApp
 
+const val LOG_TAG = "appsflyer"
 @HiltAndroidApp
-class MyShopApp : Application()
+class MyShopApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // AppsFlyer Initialization (with a sample conversion listener)
+        // Initialize AppsFlyer SDK
+        AppsFlyerLib.getInstance().init("F88pAegwy4moVzhKKspqHF", null, this)
+        AppsFlyerLib.getInstance().setDebugLog(true)
+        AppsFlyerLib.getInstance().start(
+            this, "F88pAegwy4moVzhKKspqHF",
+            object : AppsFlyerRequestListener {
+                override fun onSuccess() {
+                    Log.d(LOG_TAG, "Launch sent successfully")
+                }
+
+                override fun onError(errorCode: Int, errorDesc: String) {
+                    Log.d(
+                        LOG_TAG,
+                        "Launch failed to be sent:\n" +
+                                "Error code: " + errorCode + "\n"
+                                + "Error description: " + errorDesc,
+                    )
+                }
+            },
+        )
+    }
+}
